629859bf61687cd473a7461acec892aa75839b5f
Ingo Fischer <github@fischer-ka.de>
2017-08-28 10:20:45 +0200

    fix saveLastValuer Default

--- test/testAdapter.js
+++ test/testAdapter.js
@@ -86,7 +86,7 @@ describe('Test ' + adapterShortName + ' adapter', function() {
             // enable adapter
             config.common.enabled  = true;
             config.common.loglevel = 'debug';
-            config.common.saveLastValue = true;
+            config.native.saveLastValue = true;
 
             //config.native.dbtype   = 'sqlite';
 
