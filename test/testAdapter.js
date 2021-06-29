640de9f7db0b1b6e07731b3ecaf4a8ba843ae0b7
Ingo Fischer <github@fischer-ka.de>
2021-06-29 13:02:09 +0200

    * try convert queries to flux

--- test/testAdapter.js
+++ test/testAdapter.js
@@ -364,7 +364,7 @@ describe('Test ' + adapterShortName + ' adapter', function() {
             let query = 'SHOW FIELD KEYS FROM "influxdb.0.memRss"';
             if (process.env.INFLUXDB2) {
                 //from(db:"foo")
-                query = 'from(bucket: "iobroker") |> range(start:-1h) |> filter(fn:(r) => r._measurement == "influxdb.0.memRss") |> group(by:["_field"]) |> distinct(column:"_field") |> group(none:true)';
+                query = 'from(bucket: "iobroker") |> range(start:-1h) |> filter(fn:(r) => r._measurement == "influxdb.0.memRss") |> group(columns:["_field"]) |> distinct(column:"_field") |> group(none:true)';
             }
             sendTo('influxdb.0', 'query', query, function (result) {
                 console.log('result: ' + JSON.stringify(result.result, null, 2));
