f67976f3f7b222825be129b00423d35765a1b778
Excodibur <excodibur@posteo.de>
2021-12-14 19:28:02 +0100

    PR Review: Added suggested changes

--- main.js
+++ main.js
@@ -222,7 +222,7 @@ function stopPing(adapter) {
 }
 
 function ping(adapter) {
-    adapter._client.ping && adapter.config.pingserver && adapter._client.ping(adapter.config.pingInterval - 1000 < 0 ? 1000 : adapter.config.pingInterval - 1000)
+    adapter._client.ping && (adapter.config.pingserver !== false) && adapter._client.ping(adapter.config.pingInterval - 1000 < 0 ? 1000 : adapter.config.pingInterval - 1000)
         .then(
             hosts => {
                 if (!hosts.some(host => host.online)) {
@@ -282,7 +282,7 @@ function connect(adapter) {
             break;
     }
 
-    if (!adapter.config.pingserver)
+    if (adapter.config.pingserver === false)
         adapter.log.info("Deactivated DB health checks (ping) via configuration");
 
     adapter._client.getDatabaseNames((err, dbNames) => {
