2cc51187629da06e70830676557e3afbe4ada2e3
Ingo Fischer <github@fischer-ka.de>
2022-04-04 22:36:36 +0200

    * enhance tests

--- test/testAdapterBuffer.js
+++ test/testAdapterBuffer.js
@@ -101,7 +101,7 @@ describe(`Test ${adapterShortName} adapter with Buffered write`, function () {
             }
 
             config.native.seriesBufferFlushInterval = 30;
-            config.native.seriesBufferMax = 30;
+            config.native.seriesBufferMax = 5;
             config.native.dbname = 'otheriobroker';
 
             await setup.setAdapterConfig(config.common, config.native);
